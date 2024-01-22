3b9e8a71a7989c628c68aadac916dbb4e57964b1
Nibi5515 <111630797+Nibi5515@users.noreply.github.com>
2024-01-22 16:15:28 +0530

    My first Commit

---
+++ src/com/nibisoft/TestOne.java
@@ -0,0 +1,14 @@
+package com.nibisoft;
+
+public class TestOne {
+
+	public TestOne() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Satya");
+	}
+
+}
